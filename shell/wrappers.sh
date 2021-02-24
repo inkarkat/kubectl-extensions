463f5151ce737d6f312ea687582d3ec10d4ec4aa
Ingo Karkat <swdev@ingo-karkat.de>
2021-02-24 18:34:38 +0100

    kubectl wrapper: Return the kubectl status (Bash-only) when using pager
    set -o pipefail would also work in Korn shell, but as we're in a function the undo would be difficult. Therefore, use Bash's PIPESTATUS (sorry Korn shell; you're not my preferred shell, anyway).
    Note that for the pipeline indexing to work when eval is involved, the PIPESTATUS array has to be accessed within the eval; in other words, outside of it, eval counts as a single pipeline already.

--- shell/wrappers.sh
+++ shell/wrappers.sh
@@ -6,14 +6,15 @@
 # extensions) doesn't do that on its own.
 kubectl()
 {
+    typeset returnKubectlStatus=; [ "${BASH_VERSION:-}" ] && returnKubectlStatus='return ${PIPESTATUS[0]}'
     typeset -a pager=("${PAGER:-less}" --RAW-CONTROL-CHARS); [ -t 1 ] || pager=()
     typeset kubectlAlias="kubectl-$1"
     if [ $# -eq 0 ]; then
-	eval 'kubectl ${KUBECTL_DEFAULT_COMMAND:-p}' "${pager:+|}" '"${pager[@]}"'
+	eval 'kubectl ${KUBECTL_DEFAULT_COMMAND:-p}' "${pager:+|}" '"${pager[@]}"' "${pager:+; $returnKubectlStatus}"
     elif type ${BASH_VERSION:+-t} "$kubectlAlias" >/dev/null 2>&1; then
 	shift
-	eval $kubectlAlias '"$@"' "${pager:+|}" '"${pager[@]}"'
+	eval $kubectlAlias '"$@"' "${pager:+|}" '"${pager[@]}"' "${pager:+; $returnKubectlStatus}"
     else
-	eval 'command kubectl "$@"' "${pager:+|}" '"${pager[@]}"'
+	eval 'command kubectl "$@"' "${pager:+|}" '"${pager[@]}"' "${pager:+; $returnKubectlStatus}"
     fi
 }
