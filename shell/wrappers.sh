4b0d005e2dc57a08038c30d60a52758b5073fc44
Ingo Karkat <swdev@ingo-karkat.de>
2022-10-10 12:54:09 +0200

    Refactoring: Move the implementation from kubectl-wrapper to the kubectl() function
    So that kubectl-wrapper (or rather, clients like kcr-branch) also get the paging functionality (including its special cases), and on the other hand now the general kubectl arguments like --all-namespaces can be supplied between kubectl and the alias name.

--- shell/wrappers.sh
+++ shell/wrappers.sh
@@ -1,4 +1,4 @@
-#!/bin/sh source-this-script
+#!/bin/bash source-this-script
 
 [ "${BASH_VERSION:-}" -o "${KSH_VERSION:-}" ] || return
 
@@ -11,8 +11,22 @@ kubectl()
     let _kubectl_use+=1
     typeset returnKubectlStatus=; [ "${BASH_VERSION:-}" ] && returnKubectlStatus='return ${PIPESTATUS[0]}'
     typeset -a pager=("${PAGER:-less}" --RAW-CONTROL-CHARS); [ -t 1 ] || pager=()
+
+    typeset -a kubectlArgs=()
+    while [ $# -ne 0 ]
+    do
+	case "$1" in
+	    --all-namespaces|-A)
+			kubectlArgs+=("$1"); shift;;
+	    --namespace|-n|--selector|-l|--cluster|--context|--template)
+			kubectlArgs+=("$1" "$2"); shift; shift;;
+	    --)		break;;
+	    *)		break;;
+	esac
+    done
+
     typeset kubectlAlias="kubectl-$1"
-    if [ $# -eq 0 ]; then
+    if [ ${#kubectlArgs[@]} -eq 0 -a $# -eq 0 ]; then
 	eval 'kubectl ${KUBECTL_DEFAULT_COMMAND:-p}' "${pager:+|}" '"${pager[@]}"' "${pager:+; $returnKubectlStatus}"
     elif type ${BASH_VERSION:+-t} "$kubectlAlias" >/dev/null 2>&1; then
 	shift
@@ -20,12 +34,12 @@ kubectl()
 	[ "$kubectlAlias" = kubectl-ef ] && pager=()	# Paging would interfere with watching for changes in requested objects; turn it off.
 	[ "$kubectlAlias" = kubectl-get ] && containsGlob '-w|--watch|--watch-only' "$@" && pager=()  # Paging would interfere with watching for changes in requested objects; turn it off.
 
-	eval $kubectlAlias '"$@"' "${pager:+|}" '"${pager[@]}"' "${pager:+; $returnKubectlStatus}"
+	eval $kubectlAlias '"${kubectlArgs[@]}" "$@"' "${pager:+|}" '"${pager[@]}"' "${pager:+; $returnKubectlStatus}"
     else
 	contains logs "$@" && contains -f "$@" && pager=()  # Paging would interfere with log following; turn it off.
 	contains get "$@" && containsGlob '-w|--watch|--watch-only' "$@" && pager=()  # Paging would interfere with watching for changes in requested objects; turn it off.
 	contains edit "$@" && pager=()  # Paging would interfere with interactive editing; turn it off.
 
-	eval 'command kubectl "$@"' "${pager:+|}" '"${pager[@]}"' "${pager:+; $returnKubectlStatus}"
+	eval 'command kubectl "${kubectlArgs[@]}" "$@"' "${pager:+|}" '"${pager[@]}"' "${pager:+; $returnKubectlStatus}"
     fi
 }
